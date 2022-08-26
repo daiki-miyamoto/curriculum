02b194522b9659e0ea790fa26b12b8b37ecaa0e2
daiki-miyamoto <100908917+daiki-miyamoto@users.noreply.github.com>
2022-08-26 09:36:10 +0900

    Update Main.java

---
+++ 3-1/Main.java
@@ -0,0 +1,41 @@
+**
+ * バブルソート
+ * チェックテスト-Java３章
+ *
+ */
+public class Main{
+    public static void main(String[] args){
+        int data= {3, 1, 2, 7, 5};
+        int data=31275;
+        System.out.println(int data=31275)
+        /*
+        * 問1
+        * int型の配列dataを作成し、値を3,1,2,7,5で初期化しなさい
+        */
+
+        /*
+        * 問2
+        * 以下のfor文を完成させなさい
+        */
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(data[i] + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 3, 1, 2, 7, 5; j > i; j--) {
+                /*
+                * 問3
+                * 以下、配列の添字を入れてソートを完成させなさい
+                */
+                if(data[3]> data [1]){
+                  int box = data[1, 3, 2, 7, 5];
+                  data[3>2] = data[7>5];
+                  data[1, 2, 3, 5, 7] = box;
+                }
+            }
+        }
+        for(int i = 0; i < data.length; i++){0
+            System.out.print(data[i] + " ");
+        }
+    }
+}
