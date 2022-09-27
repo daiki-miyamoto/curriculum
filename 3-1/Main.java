3bcd4bc4c19450790acf6df26c9e43188a7707f3
daiki-miyamoto <100908917+daiki-miyamoto@users.noreply.github.com>
2022-09-28 00:29:21 +0900

    Update Main.java

--- 3-1/Main.java
+++ 3-1/Main.java
@@ -9,7 +9,6 @@ public class Main{
         * 問1
         * int型の配列dataを作成し、値を3,1,2,7,5で初期化しなさい
         */
-        int data=31275;
         int[]data={3,1,2,7,5}
         System.out.println(data);
 
@@ -22,15 +21,15 @@ public class Main{
         }
         System.out.println();
         for (int i = 0; i < data.length; i++) {
-            for (int j = 4; j > i; j--) {
+            for (int j = i; j > i; j--) {
                 /*
                 * 問3
                 * 以下、配列の添字を入れてソートを完成させなさい
                 */
-                if(data[4]> data [0]){
-                  int box = data[3, 1, 2, 7, 5];
-                  data[4] = data[4];
-                  data[0, 1, 2, 3, 4] = box;
+                if(data[i]> data [j]){
+                  int box = data[i];
+                  data[i] = data[j];
+                  data[j] = box;
                 }
             }
         }
