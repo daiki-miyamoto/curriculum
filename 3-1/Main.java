09716ccd3a715fb309054994ecc2631afbfbbe12
daiki-miyamoto <100908917+daiki-miyamoto@users.noreply.github.com>
2022-10-11 13:56:15 +0900

    Update Main.java

--- 3-1/Main.java
+++ 3-1/Main.java
@@ -1,4 +1,4 @@
-**
+/*
  * バブルソート
  * チェックテスト-Java３章
  *
@@ -9,7 +9,7 @@ public class Main{
         * 問1
         * int型の配列dataを作成し、値を3,1,2,7,5で初期化しなさい
         */
-        int[]data={3,1,2,7,5}
+        int[]data={3,1,2,7,5};
         System.out.println(data);
 
         /*
@@ -21,7 +21,7 @@ public class Main{
         }
         System.out.println();
         for (int i = 0; i < data.length; i++) {
-            for (int j = i; j > i; j--) {
+            for (int j = 4; j > i; j--) {
                 /*
                 * 問3
                 * 以下、配列の添字を入れてソートを完成させなさい
